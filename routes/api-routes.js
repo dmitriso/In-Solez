a8fa91ad193ee8c92b5a0c911d86d2b463630476
Dmitri So <dmitriso@yahoo.com>
2021-01-16 14:22:14 -0500

    Created t=route to post a new User to the database.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,15 +2,31 @@
 const db = require("../models");
 const passport = require("../config/passport");
 
-module.exports = function(app) {
-  // THIS AUTHENTICATES THE USERS LOGIN NAME AND PASSWORD.
+module.exports = function (app) {
+  // THIS AUTHENTICATES THE USERS LOGIN NAME AND PASSWORD
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
     res.json({
       userName: req.user.userName,
       id: req.user.id
     });
   });
-
+  // THIS ROUTE CREATES A NEW USER ROW 
+  app.post("/api/signup", (req, res) => {
+    var user = req.body;
+    db.User.create({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      password: user.password,
+      email: user.email
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch(err => {
+        res.status(401).json(err);
+      });
+  });
 
 
 
