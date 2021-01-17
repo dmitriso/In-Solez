c2be94fabc1d0c60653623e00ed3e6d39534ba7a
wylie <wyliedavid1984@gmail.com>
2021-01-16 21:54:48 -0500

    added in search get request for search page

--- public/js/members.js
+++ public/js/members.js
@@ -3,21 +3,24 @@ $(document).ready(() => {
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.userName);
-    $.get("https://api.thesneakerdatabase.com/v1/sneakers?limit=30").then(
-      sneakerData => {
-        console.log(sneakerData.results);
-
-        sneakerData.results.forEach(sneaker => {
-          const $clone = $("#sneaker")
-            .clone()
-            .removeAttr("id");
-          $clone.find(".sneakerName").text(sneaker.name);
-          $clone.find(".brand").text(sneaker.brand);
-          $clone.find(".shoeName").text(sneaker.shoe);
-          $clone.find("img").prop("src", sneaker.media.thumbUrl);
-          $clone.appendTo("#sneakers");
-        });
-      }
-    );
+    const randomNum = Math.floor(Math.random() * 350 + 1);
+    $.get(
+      "https://api.thesneakerdatabase.com/v1/sneakers?limit=30&page=" +
+        randomNum
+    ).then(sneakerData => {
+      console.log(sneakerData.results);
+      sneakerData.results.forEach(sneaker => {
+        const $clone = $("#sneaker")
+          .clone()
+          .removeAttr("id");
+        $clone.find(".sneakerName").text(sneaker.name);
+        $clone.find(".brand").text(sneaker.brand);
+        $clone.find(".shoeName").text(sneaker.shoe);
+        $clone.find("img").prop("src", sneaker.media.thumbUrl);
+        $clone.appendTo("#sneakers");
+      });
+    });
   });
+
+  
 });
