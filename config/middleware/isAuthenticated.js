8c5f6fe8d588d2d75ec5fa3296f72dd30901e0f1
wylie <wyliedavid1984@gmail.com>
2021-01-18 17:49:26 -0500

    added in a new file for isNotAuthenticated

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,24 +1,10 @@
 // This is middleware for restricting routes a user is not allowed to visit if not logged in
-module.exports = class Auth {
+module.exports = function(req, res, next) {
   // If the user is logged in, continue with the request to the restricted route
-  checkAuthentication = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      //req.isAuthenticated() will return true if user is logged in
-     return next();
-    } else {
-      res.redirect("/login");
-    }
-  };
-
-  notAuthentication = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      //req.isAuthenticated() will return true if user is logged in
-      res.redirect("/profile");
-    } else {
-     return next();
-    }
-  };
+  if (req.user) {
+    return next();
+  }
 
   // If the user isn't logged in, redirect them to the login page
-
+  return res.redirect("/login");
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,47 +1,44 @@
 // Requiring our custom middleware for checking if a user is logged in
-const Auth = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+const isNotAuthenticated = require("../config/middleware/isNotAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
-    // if (req.user) {
-    //  
-     res.redirect("/profile");
-    // }
+    if (req.user) {
+      res.redirect("/profile");
+    }
     res.render("home", { title: "InSolez||Homepages" });
   });
 
-  app.get("/login", Auth.notAuth, (req, res) => {
+  app.get("/login", isNotAuthenticated, (req, res) => {
     // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/profile");
-    // }
+    if (req.user) {
+      res.redirect("/profile");
+    }
     res.render("login", { title: "InSolez||Login" });
   });
 
-  app.get("/search", Auth.checkAuth, (req, res) => {
+  app.get("/search", (req, res) => {
     // If the user already has an account send them to the members page
-    res.render("search", { title: "InSolez||Search" });
+    res.render("search", {
+      title: "InSolez||Search"
+    });
   });
-
-  // this path is in twice didn't want to delete but wanted to make sure it wasn't here for a reason
-  // app.get("/profile", (req, res) => {
-  //   // If the user already has an account send them to the members page
-  //   res.render("profile", { title: "InSolez||Profile" });
-  // });
-
-  app.get("/signup", (req, res) => {
+  
+  app.get("/signup", isNotAuthenticated, (req, res) => {
     // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/profile");
-    // }
-    res.render("signup", Auth.notAuth, {
+    if (req.user) {
+      res.redirect("/profile");
+    }
+    res.render("signup", {
       title: "InSolez||Signup"
     });
   });
+  
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/profile", Auth.checkAuth, (req, res) => {
+  app.get("/profile", isAuthenticated, (req, res) => {
     res.render("profile", { title: "InSolez||Profile" });
   });
 };
