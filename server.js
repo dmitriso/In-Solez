35e3633d1b8aa7a4f73880b361342713c20f698b
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-17 23:58:54 -0500

    Set up handlebars

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// This sets up the Express Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
