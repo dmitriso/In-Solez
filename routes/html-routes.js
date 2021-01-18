5f429f37af3f828cc1f48daac6b97c9e09292a4b
wylie <wyliedavid1984@gmail.com>
2021-01-18 17:31:11 -0500

    added in a notauthenitacted functions and add them to html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,17 @@
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
+const Auth = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     // if (req.user) {
-    //   res.redirect("/profile");
+    //  
+     res.redirect("/profile");
     // }
     res.render("home", { title: "InSolez||Homepages" });
   });
 
-  app.get("/login", (req, res) => {
+  app.get("/login", Auth.notAuth, (req, res) => {
     // If the user already has an account send them to the members page
     // if (req.user) {
     //   res.redirect("/profile");
@@ -18,28 +19,29 @@ module.exports = function(app) {
     res.render("login", { title: "InSolez||Login" });
   });
 
-  app.get("/search", (req, res) => {
+  app.get("/search", Auth.checkAuth, (req, res) => {
     // If the user already has an account send them to the members page
     res.render("search", { title: "InSolez||Search" });
   });
 
-  app.get("/profile", (req, res) => {
-    // If the user already has an account send them to the members page
-    res.render("profile", { title: "InSolez||Profile" });
-  });
+  // this path is in twice didn't want to delete but wanted to make sure it wasn't here for a reason
+  // app.get("/profile", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   res.render("profile", { title: "InSolez||Profile" });
+  // });
 
   app.get("/signup", (req, res) => {
     // If the user already has an account send them to the members page
     // if (req.user) {
     //   res.redirect("/profile");
     // }
-    res.render("signup", {
+    res.render("signup", Auth.notAuth, {
       title: "InSolez||Signup"
     });
   });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/profile", isAuthenticated, (req, res) => {
+  app.get("/profile", Auth.checkAuth, (req, res) => {
     res.render("profile", { title: "InSolez||Profile" });
   });
 };
--- routes/login-api-routes.js
+++ routes/login-api-routes.js
@@ -23,23 +23,23 @@ module.exports = function (app) {
   );
   // THIS ROUTE CREATES A NEW USER ROW
   app.post("/api/signup", async (req, res) => {
-      try {
-        const user = req.body;
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        db.User.create({
-          firstName: user.firstName,
-          lastName: user.lastName,
-          userName: user.userName,
-          password: hashedPassword,
-          email: user.email
-        });
-        console.log(data.toString())
-        res.status(307).end();
-      } catch {
-        res.json(err);
-        res.status(401).redirect("/signup")
-      }
-    });
+    try {
+      const user = req.body;
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      db.User.create({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userName: user.userName,
+        password: hashedPassword,
+        email: user.email
+      });
+      console.log(data.toString())
+      res.status(307).end();
+    } catch {
+      res.json(err);
+      res.status(401).redirect("/signup")
+    }
+  });
 
   // ROUTE FOR LOGGING USER OUT
   app.get("/logout", (req, res) => {
@@ -57,7 +57,13 @@ module.exports = function (app) {
       });
     }
   });
-
+  // checks to see if the user is logged 
+  // checkAuthenticated = ((req, res, next) => {
+  //   if (req.isAuthenticated()) {
+  //     next();
+  //   }
+  //   res.redirect("/login")
+  // });
   // router.get("/logout",(req,res) => {
   //   req.logout();
   //   req.session.destroy(() => {
