ffc559a92853aae516eb1c09a79eee8bd02bdb58
wylie <wyliedavid1984@gmail.com>
2021-01-22 08:34:27 -0500

    quick edits

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -3,14 +3,14 @@ $(document).ready(() => {
   $(document).on("click", ".collectionFavorite-btn", function(e) {
     e.preventDefault();
     $(this).toggleClass("toggle");
-    // addFavorite(this.id);
+    addFavorite(this.id);
   });
 });
 
 function addFavorite(id) {
   $.ajax({
     method: "PUT",
-    url: "/api/sneaker/favorite" + id
+    url: "/api/sneaker/favorite/" + id
   })
     .then(response => {
       console.log(response);
